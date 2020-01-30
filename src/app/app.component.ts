ad1560c5b4223600ceb8818f5b3d630ba11256db
Lucas Maciel <lucasmacielgois@gmail.com>
2020-01-30 09:17:41 -0300

    navegacao a partir do menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -9,10 +9,11 @@ import { ListaAgendamentosPage } from '../pages/lista-agendamentos/lista-agendam
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) public nav: Nav;
   rootPage:any = HomePage;
 
    public paginas = [
-    { titulo: 'Agendamentos' , pagina: ListaAgendamentosPage.name, icone: 'calendar'}
+    { titulo: 'Agendamentos' , componente: ListaAgendamentosPage.name, icone: 'calendar'}
    ];
 
 
@@ -22,5 +23,14 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+  irParaPagina(componente) {
+    this.nav.push(componente);
+  }
+
+
+
+
+
 }
 
