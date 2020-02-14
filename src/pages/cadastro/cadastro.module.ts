36605154851e96499372318ce8948e554e1d13bf
Lucas Maciel <lucasmacielgois@gmail.com>
2020-02-14 16:43:18 -0300

    internacionalizacao

--- src/pages/cadastro/cadastro.module.ts
+++ src/pages/cadastro/cadastro.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CadastroPage } from './cadastro';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { CadastroPage } from './cadastro';
   ],
   imports: [
     IonicPageModule.forChild(CadastroPage),
+    TranslateModule.forChild()
   ],
   exports: [
     CadastroPage // sempre que criar uma página, criar o exports da propria pagina, para que possa ser acessado
